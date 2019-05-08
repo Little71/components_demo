e503ec0772557ba6b92cebab280a363f3f0a3ad8
mr7 <596786272@qq.com>
2019-05-08 14:18:26 +0800

    markdemo

--- vue_work/src/main.js
+++ vue_work/src/main.js
@@ -7,37 +7,42 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-var data=[
-  {id:1,title:'标题1',content:'内容1'},
-  {id:2,title:'标题2',content:'内容2'},
-  {id:3,title:'标题3',content:'内容3'},
-  {id:4,title:'标题4',content:'内容4'},
+var data = [
+  { id: 1, title: '标题1', content: '内容1' },
+  { id: 2, title: '标题2', content: '内容2' },
+  { id: 3, title: '标题3', content: '内容3' },
+  { id: 4, title: '标题4', content: '内容4' },
 ]
 
 const store = new Vuex.Store({
-  state:{
-    alllist:[],
+  state: {
+    alllist: [],
     note: {
       title: '',
       content: '',
       // markdown: '',
     }
   },
-  mutations:{
-    GETALLLIST(state,newdata){
+  mutations: {
+    GETALLLIST(state, newdata) {
       state.alllist = newdata;
     },
-    ADDONENOTE(state,newdata){
+    ADDONENOTE(state, newdata) {
       state.alllist = newdata;
     },
   },
-  actions:{
-    getAllList(context){
-      context.commit('GETALLLIST',data)
+  actions: {
+    getAllList(context) {
+      context.commit('GETALLLIST', data)
+      // axios.get('url').then(function (response) { 
+      //   console.log(response);
+      // }).catch(function (error) { 
+      //   console.log(error);
+      // });
     },
-    addOneNote(context,json){
+    addOneNote(context, json) {
       data.push(json)
-      context.commit('ADDONENOTE',data)
+      context.commit('ADDONENOTE', data)
     }
   }
 });
